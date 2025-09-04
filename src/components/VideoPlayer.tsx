a1e4f745726a2c13747ca15b071efef0f855ae09
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2025-09-04 20:42:10 +0200

    Add text with a translucent overlay

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -20,6 +20,17 @@ export const VideoPlayer = ({ src }: Props) => {
           className="h-[900px] w-full object-cover bg-black-40"
         />
       </div>
+      <div className="absolute inset-0 -z-10 bg-black/40" />
+      <div className="mx-auto flex h-[900px] max-w-7xl flex-col justify-end pb-40">
+        <div className="max-w-4xl mb-10 text-[45px] leading-[57px]">
+          <h1 className="font-semibold tracking-tight mb-2">
+            Lorem Ipsum Dolor Sit amet
+          </h1>
+          <h2 className="text-[#ECECEC] font-semibold">
+            Donec cursus ligula diam, nec congue augue ultrices nec.
+          </h2>
+        </div>
+      </div>
     </section>
   );
 };
