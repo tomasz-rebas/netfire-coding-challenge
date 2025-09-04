4a55c7f4d4e4717195455a4cb89fdab61891d44f
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2025-09-04 21:27:31 +0200

    Add navigation buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,17 @@ export const Header = () => {
         <span className="text-[#404F84]">WELCOME15</span>
       </div>
       <div className="grid grid-cols-3 max-w-7xl mx-auto h-26">
-        <nav></nav>
+        <nav className="flex gap-4 justify-start items-center">
+          <button className="border-[#827D7D] border-solid border-1 bg-[#E5E5DF] py-2 px-6 rounded-lg text-center">
+            HOME
+          </button>
+          <button className="border-[#827D7D] border-solid border-1 bg-[#D9D9D9] py-2 px-6 rounded-lg text-center">
+            ABOUT
+          </button>
+          <button className="border-[#827D7D] border-solid border-1 bg-[#D9D9D9] py-2 px-6 rounded-lg text-center">
+            CONTACT
+          </button>
+        </nav>
         <div className="flex justify-center items-center">
           <BrandLogo />
         </div>
