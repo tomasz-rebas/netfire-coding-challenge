40256be547af6fd848dc187593e4aa952be218cf
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2025-09-04 22:01:52 +0200

    Implement the section above the footer

--- src/components/ExampleSection.tsx
+++ src/components/ExampleSection.tsx
@@ -1,3 +1,20 @@
-export const ExampleSection = () => {
-  return <div>Example Section</div>;
-};
+export const ExampleSection = () => (
+  <div className="flex justify-center py-20">
+    <div className="max-w-7xl text-center">
+      <button className="border-[#443B3B] border-solid border-1 bg-[#D9D9D930] px-6 py-4 rounded-xl text-xs">
+        EXAMPLE SECTION
+      </button>
+      <p className="text-[#1C1B1B] text-[32px] leading-[40px] my-8 tracking-tight">
+        Lorem{" "}
+        <span className="text-gradient font-semibold">
+          ipsum dolor sit amet consectetur adipiscing elit etiam molestie ex vel
+        </span>{" "}
+        massa lacinia ultricies. Suspendisse lobortis vehicula eros, sed sodales
+        eros mollis eget. Morbi vitae libero in nunc sodales.
+      </p>
+      <div className="text-sm text-[#41423E] font-semibold underline">
+        SEE MORE
+      </div>
+    </div>
+  </div>
+);
