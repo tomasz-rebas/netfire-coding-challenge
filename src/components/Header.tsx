359e5a620d2e9a00362860ad63a6cc7a23096256
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2025-09-04 21:24:52 +0200

    Add discount banner

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,10 @@
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="w-full">
+      <div className="text-center bg-[#D9D9D9] text-[#111010] py-6">
+        NEW CUSTOMERS GET 15% OFF! USE CODE{" "}
+        <span className="text-[#404F84]">WELCOME15</span>
+      </div>
+    </header>
+  );
 };
